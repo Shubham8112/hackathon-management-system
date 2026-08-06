50a4211ac70caa44128276d5fd9bfc8e890551a4
Shubham Pandey <shubpandeyham@gmail.com>
2026-08-06 14:37:30 +0530

    Sign up page

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
 import './Signup.css';
+import api from "../../services/api";
+
 function Signup() {
     const [name, setName] = useState('');
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
+    const[role, setRole] = useState("participant");
+    const Navigate = useNavigate();
 
+    const handleSignup = async() =>{
+      try{
+        const response = await api.post("/auth/signup",{
+          name,
+          email,
+          password,
+          role,
+        });
+
+        alert(response.data.message);
+        Navigate("/login");
+      }catch(error){
+        alert(error.response?.data?.message || "Signup Failed");
+      }
+    }
   return (
     <div className='main'>
         <div className="container">
@@ -27,8 +46,15 @@ function Signup() {
             value = {password}
             onChange={(e) => setPassword(e.target.value)}
              />  
-            <button>Register</button>
+            
+            <select value={role} onChange={(e)=> setRole(e.target.value)}>
+              <option value="participant">Participant</option>
+              <option value="organizer">Organizer</option>
+            </select>
+            <button onClick={handleSignup}>Sign Up</button>
+
             <Link to="/login">Already have an account?</Link>
+
         </div>
     </div>
   )
